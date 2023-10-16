cd6416ffca1d4ce5f3fdffa6406d0ce0dfb24e17
sashasashkina <koshyl.apps@gmail.com>
2023-10-16 15:22:32 +0300

    3

--- js/task-02.js
+++ js/task-02.js
@@ -12,7 +12,7 @@ const ingredients = [
 // Додасть назву інгредієнта як його текстовий вміст.
 // Додасть елементу клас item.
 // Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
-const getIngEl = document.createElement("li");
+
 const ingredientsRef = document.querySelector("#ingredients");
 
 const itemsRef = ingredients.map((ingredient) => {
